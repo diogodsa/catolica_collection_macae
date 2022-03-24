3fb83c4df88b3754aaca1452677f1e3031a82ab9
Marcelo Paes <marcelocastrops@gmail.com>
2022-03-24 20:52:43 -0300

    Second commit

---
+++ src/main/java/com/collection/collection/Carro.java
@@ -0,0 +1,10 @@
+package com.collection.collection;
+
+public class Carro {
+
+    String fabricante;
+    String modelo;
+    String cor;
+    int anoDeFabricacao;
+
+}
--- src/main/java/com/collection/collection/CollectionApplication.java
+++ src/main/java/com/collection/collection/CollectionApplication.java
@@ -1,9 +1,11 @@
 package com.collection.collection;
 
+import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @SpringBootApplication
@@ -15,17 +17,22 @@ public class CollectionApplication {
 
 	@RequestMapping("/")
 	String home(){
-		return "Hello Word!";
+		Carro meuCarro = new Carro();
+		meuCarro.anoDeFabricacao = 2022;
+		meuCarro.cor = "Branco";
+		meuCarro.modelo = "JEEP";
+		meuCarro.fabricante = "JEEP";
+
+		Gson gson = new Gson();
+		String json = gson.toJson(meuCarro);
+
+		return json;
 	}
 
-	@RequestMapping("/v1/user")
-	String testeUsuario(){
-		return "Hello User!";
+	@RequestMapping("/v1/user/Thiago")
+	String thiago(){
+		return "Hello Thiago :)";
 	}
 
-	@RequestMapping("/v1/user/thiago")
-	String testeThiago(){
-		return "Hello Class!";
-	}
 
 }
